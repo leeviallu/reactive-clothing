ea31c2be2f7e7113e482c5666ccca632fbfb03ba
leeviallu <68004427+leeviallu@users.noreply.github.com>
2022-04-19 16:02:57 +0300

    added routes and sign in component

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,63 @@
+import { initializeApp } from 'firebase/app';
+import { 
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider 
+} from 'firebase/auth';
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZMMXA1bDd1OzkmKYchO3mjo-jNThSAhg",
+    authDomain: "reactive-clothing-db.firebaseapp.com",
+    projectId: "reactive-clothing-db",
+    storageBucket: "reactive-clothing-db.appspot.com",
+    messagingSenderId: "967260088537",
+    appId: "1:967260088537:web:a7405e192d34d9bd01b6c8"
+  };
+  
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    } 
+
+    return userDocRef;
+
+}
