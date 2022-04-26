a143f9177c4e6d07bb337449ef76c69213b38aa6
leeviallu <68004427+leeviallu@users.noreply.github.com>
2022-04-27 02:19:17 +0300

    addToCart feature added

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { ProductsContext } from '../../contexts/products.context';
 import ProductCard from '../../components/product-card/product-card.component';
 
-import './shop.styles.scss';
+import './shop.styles.scss'; 
 
 const Shop = () => {
     const {products} = useContext(ProductsContext);
